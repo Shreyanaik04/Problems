6ec6cc92f582e759b4929ed7258ea46ea87c5c43
Shreya Naik <136881044+Shreyanaik04@users.noreply.github.com>
2023-08-17 20:48:48 +0530

    Add files via upload

---
+++ CalculatorUsingSwitch.java
@@ -0,0 +1,56 @@
+package com.Shreya;
+
+import java.util.Scanner;
+
+public class CalculatorUsingSwitch {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double num1 , num2 , answer;
+        String choice;
+        while (true){
+            System.out.println("Select operator: ");
+            char operator = in.next().charAt(0);
+            System.out.println("Enter num1: ");
+            num1 = in.nextInt();
+            System.out.println("Enter num2: ");
+            num2 = in.nextInt();
+            switch (operator){
+            case '+':
+                answer = num1 + num2;
+                System.out.println("answer = "+ answer);
+                break;
+            case '-':
+                answer = num1 - num2;
+                System.out.println("answer = "+ answer);
+                break;
+            case '/':
+                answer = num1 / num2;
+                System.out.println("answer = "+ answer);
+                break;
+            case '%':
+                answer = num1 % num2;
+                System.out.println("answer = "+ answer);
+                break;
+            case '*':
+                answer = num1 * num2;
+                System.out.println("answer = "+ answer);
+                break;
+            default:
+                System.out.println("Please enter a valid operator");
+                continue;
+            }
+            System.out.println("Select Y / N ");
+            choice = in.next();
+
+            if (choice.equalsIgnoreCase("Y")) {
+                System.out.println("Continued!!");
+            } else if (choice.equalsIgnoreCase("N")) {
+                System.out.println("Thanks for using Calculator!!");
+                System.exit(0);
+            } else {
+                System.out.println("Invalid input. Please try again...");
+            }
+        }
+
+    }
+}
