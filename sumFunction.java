ee7e2ed85622f1baa2928a076c09a72c7c476f17
Shreya Naik <136881044+Shreyanaik04@users.noreply.github.com>
2023-08-19 00:28:55 +0530

    Add files via upload

---
+++ sumFunction.java
@@ -0,0 +1,31 @@
+package com.Shreya;
+
+import java.util.Scanner;
+
+public class sumFunction {
+    public static void main(String[] args) {
+        sum();
+    }
+    static void sum() {
+        Scanner in = new Scanner(System.in);
+        while (true){
+            System.out.print("Enter num1: ");
+            int num1 = in.nextInt();
+            System.out.print("Enter num2: ");
+            int num2 = in.nextInt();
+            int sum = num1 + num2;
+            System.out.println("Sum of the numbers = " +sum);
+            System.out.println("Do you want to keep adding ? Y/N");
+            String choice = in.next();
+            if(choice.equalsIgnoreCase("Y")){
+                System.out.println("Continued");
+            } else if (choice.equalsIgnoreCase("N")) {
+                System.out.println("Thank you!");
+                System.exit(0);
+            }else {
+                System.out.println("Please select valid choice!!");
+            }
+        }
+
+    }
+}
